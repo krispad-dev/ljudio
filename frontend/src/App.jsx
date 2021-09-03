8e37b031a59b37d2930cf3a7bc1b0bc045d89102
Chris <chrisgbg@protonmail.com>
2021-09-03 13:47:07 +0200

    Frontend boilerplate

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import './App.css';
+
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Route path="/profile" component={ProfilePage} />
+    </div>
+  );
+}
+
+export default App;
+
+// rfce för komponent
+// semikolon & single quote
+// minst en rad mellan kodblock
