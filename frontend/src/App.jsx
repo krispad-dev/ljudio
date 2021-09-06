8f1ce6bd5f59abac121402d2313b4060be71e467
Adam-Bjurhede <adambjurhede@gmail.com>
2021-09-06 10:46:31 +0200

    Made register page with validation form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,26 @@
 import React, { useState } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import RegisterPage from './pages/RegisterPage';
 
 import './App.css';
 
 function App() {
-	return (
-		<div className='App'>
-			<header>
-				<h1>LJUDIO</h1>
-			</header>
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <header>
+          <h1>LJUDIO</h1>
+          <Link to='/register'>REGISTER</Link>
+        </header>
 
-      <aside>
-
-      </aside>
-
-			<main>
-
-			</main>
-
-			<footer></footer>
-		</div>
-	);
+        <aside></aside>
+        <main>
+          <Route path='/register' component={RegisterPage}></Route>
+        </main>
+        <footer></footer>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/RegisterPage.jsx
@@ -0,0 +1,153 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import styled from 'styled-components';
+import { yupResolver } from '@hookform/resolvers/yup';
+// import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+
+//Validation Schema
+const schema = yup.object().shape({
+  userName: yup.string().required(),
+  email: yup.string().email().required(),
+  password: yup.string().min(6).max(32).required(),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref('password'), null], 'Password must match'),
+});
+
+function RegisterPage() {
+  const history = useHistory();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmitHandler = (data) => {
+    console.log(data);
+    reset();
+  };
+
+  return (
+    <RegisterPageWrapper>
+      {/* <Link to='/' className='close-register'>
+        X
+      </Link> */}
+      <a onClick={() => history.goBack()} className='close-register'>
+        X
+      </a>
+      <div className='register-page-contentWrapper'>
+        <h1 className='register-heading'>REGISTER</h1>
+        <form
+          onSubmit={handleSubmit(onSubmitHandler)}
+          className='register-form'
+          action=''
+        >
+          <input
+            {...register('userName')}
+            type='text'
+            name='userName'
+            placeholder='Username'
+          />
+          <p>{errors.userName?.message}</p>
+          <input
+            {...register('email')}
+            type='email'
+            name='email'
+            placeholder='Email'
+          />
+          <p>{errors.email?.message}</p>
+          <input
+            {...register('password')}
+            type='password'
+            name='password'
+            placeholder='Password'
+          />
+          <p>{errors.password?.message}</p>
+          <input
+            {...register('confirmPassword')}
+            type='password'
+            name='confirmPassword'
+            placeholder='Confirm Password'
+          />
+          <p>{errors.confirmPassword?.message}</p>
+          <button type='submit'>SUBMIT</button>
+        </form>
+      </div>
+    </RegisterPageWrapper>
+  );
+}
+
+export default RegisterPage;
+
+const RegisterPageWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+  background-color: #131313;
+
+  .close-register {
+    position: absolute;
+    top: 30px;
+    right: 30px;
+    color: white;
+    font-size: 1rem;
+    padding: 7px 10px;
+    border: 1px solid white;
+    border-radius: 50%;
+
+    text-align: center;
+  }
+  .register-page-contentWrapper {
+    max-width: 700px;
+    margin: auto auto;
+    padding: 10%;
+  }
+
+  .register-heading {
+    font-size: 4rem;
+    text-align: center;
+    font-weight: lighter;
+    margin-bottom: 10%;
+  }
+
+  .register-form {
+    max-width: 300px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+
+    align-items: center;
+
+    input {
+      width: 100%;
+      text-align: center;
+      padding-top: 14px;
+      padding-bottom: 14px;
+      border-radius: 5px;
+    }
+
+    button {
+      width: 100%;
+      color: white;
+      background-color: black;
+      border: 1px solid rgb(207, 203, 203);
+      padding: 14px;
+      font-size: 0.6rem;
+      font-weight: lighter;
+      border-radius: 5px;
+    }
+
+    p {
+      color: white;
+    }
+  }
+`;
