efb69d3ec02c8d08b2080af6d7a27dd83e8c66c4
Chris <chrisgbg@protonmail.com>
2021-09-10 13:27:28 +0200

    Added playlistController and created route for createPlaylist

---
+++ backend/controllers/playlistController.js
@@ -0,0 +1,25 @@
+import { nanoid } from 'nanoid';
+
+export async function createPlaylist(req, res) {
+  try {
+    // Each playlist getting an unique ID
+    const id = nanoid();
+
+    // ID of the currently logged in user
+    const userId = req.obj.id;
+
+    // Title for the playlist
+    const title = req.body.title;
+
+    // Complete playlist object to store in database
+    const playList = { id, userId, title };
+
+    // Store playlist in database
+    await PlayLists.createPlaylist(playList);
+
+    return res.status(201).json({ success: true });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ success: false });
+  }
+}
