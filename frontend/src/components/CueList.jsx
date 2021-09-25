71028a120548f585f56f12587f0d2e09de4bd86a
Chris <chrisgbg@protonmail.com>
2021-09-25 16:12:33 +0200

    Added component for CueSongItem

--- frontend/src/components/CueList.jsx
+++ frontend/src/components/CueList.jsx
@@ -1,8 +1,9 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { UiContext } from '../context/UiState';
 import { playerControllerStateContext } from '../context/YouTubePlayerContext';
 import { PLAYER_ACTIONS } from '../reducers/YouTubePlayerReducer';
 import SongCardItem from '../components/OnePlaylistPage/SongCardItem';
+import CueSongCardItem from './CueSongCardItem';
 import useGetAllCued from '../hooks/useGetAllCued';
 import AddMusicToPlayListList from './AddMusicToPlaylistMenu/AddMusicToPlayListList';
 import styled from 'styled-components';
@@ -15,7 +16,40 @@ function CueList() {
     const { data } = useGetAllCued();
     const [ { pendingUserCue }, dispatch ] = useContext(playerControllerStateContext);
 
+    const [uiSongs, setUiSongs] = useState([]);
+
     const songs = data?.cue.map(({videoId}) => videoId) || [];
+
+    function onDragOver(e) {
+        e.preventDefault();
+    }
+    
+    function onDrop(e) {
+        const data = e.dataTransfer.getData('text/plain');
+        console.log(data);
+        console.log('Dropped!');
+    }
+    
+    function onDragStart(e, id) {
+        console.log(id);
+        e.dataTransfer.setData('text/plain', id);
+    }
+    
+    function testDrop(e, index) {
+        const defaultEl = index;
+        const newEl = e.dataTransfer.getData('text/plain');
+    
+        console.log('default:', uiSongs[defaultEl]);
+        console.log('new:', uiSongs[newEl]);
+        
+        const newArr = [ ...uiSongs ];
+    
+        const element = uiSongs[newEl];
+        newArr.splice(newEl, 1);
+        newArr.splice(defaultEl, 0, element);
+    
+        setUiSongs(newArr);
+    }
 
     useEffect(() => {
 
@@ -26,10 +60,27 @@ function CueList() {
         
     }, [data]);
 
+    useEffect(() => {
+
+        if(data) {
+          setUiSongs(songs);
+        }
+    
+    }, [data]);
+
     return (
-        <CueListWrapper>
+        <CueListWrapper onDragOver={(e) => onDragOver(e)}>
             {state.saveSongToPlaylistSelectorSectionIsOpen && <AddMusicToPlayListList />}
-            {data && data.cue.map((song, i) => <SongCardItem key={song.id} song={song.videoId} cueId={song.id} index={i} />)}
+            {uiSongs.map((song, i) => {
+                return <CueSongCardItem 
+                        key={song} 
+                        song={song} 
+                        cueId={song.id} 
+                        index={i} 
+                        onDragStart={onDragStart}
+                        onDrop={testDrop}
+                        />
+            })}
         </CueListWrapper>
     );
 }
