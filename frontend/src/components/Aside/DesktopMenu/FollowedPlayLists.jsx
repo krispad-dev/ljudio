eaf9ff4eeb8dbd22d400ec675f0b121275666c56
Adam-Bjurhede <adambjurhede@gmail.com>
2021-09-27 09:51:47 +0200

    switch key-prop to index in followedPlaylists.jsx

--- frontend/src/components/Aside/DesktopMenu/FollowedPlayLists.jsx
+++ frontend/src/components/Aside/DesktopMenu/FollowedPlayLists.jsx
@@ -4,42 +4,41 @@ import styled from 'styled-components';
 import useGetFollowedPlaylists from '../../../hooks/useGetFollowedPlaylists';
 
 function FollowedPlaylists() {
+  const { data } = useGetFollowedPlaylists();
 
-	const { data } = useGetFollowedPlaylists();
-
-	return (
-		<FollowingPlayListWrapper>
-			{data &&
-				data.followedPlaylists &&
-				data.followedPlaylists.length > 0 &&
-				data.followedPlaylists.map(playlist => {
-					return <PlayListCardItem  key={playlist.id} {...playlist} followItem={true} />;
-				})}
-			{data && data.followedPlaylists && data.followedPlaylists.length === 0 && <p>No followed lists yet =/</p>}
-		</FollowingPlayListWrapper>
-	);
+  return (
+    <FollowingPlayListWrapper>
+      {data &&
+        data.followedPlaylists &&
+        data.followedPlaylists.length > 0 &&
+        data.followedPlaylists.map((playlist, i) => {
+          return <PlayListCardItem key={i} {...playlist} followItem={true} />;
+        })}
+      {data && data.followedPlaylists && data.followedPlaylists.length === 0 && <p>No followed lists yet =/</p>}
+    </FollowingPlayListWrapper>
+  );
 }
 
 const FollowingPlayListWrapper = styled.ul`
-	animation: dropDown ease-in-out 0.2s;
-	@keyframes dropDown {
-		from {
-			height: 0%;
-		}
+  animation: dropDown ease-in-out 0.2s;
+  @keyframes dropDown {
+    from {
+      height: 0%;
+    }
 
-		to {
-			height: 100%;
-		}
-	}
+    to {
+      height: 100%;
+    }
+  }
 
-	height: 100%;
-	::-webkit-scrollbar {
-		display: none;
-	}
-	overflow-y: scroll;
-	color: #c4c4c4;
-	margin-bottom: 10px;
-	width: 100%;
+  height: 100%;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  overflow-y: scroll;
+  color: #c4c4c4;
+  margin-bottom: 10px;
+  width: 100%;
 `;
 
 export default FollowedPlaylists;
