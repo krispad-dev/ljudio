fdf9e393265e53d2676352a1f9c043661f5787a7
krispad-dev <kristofer.padoan@icloud.com>
2021-09-27 16:51:55 +0200

    responisve footer & player controller

--- frontend/src/components/YoutubePlayer/ProgressSlider.jsx
+++ frontend/src/components/YoutubePlayer/ProgressSlider.jsx
@@ -45,7 +45,7 @@ const VolumeSliderContainer = styled.div`
 	display: flex;
 	justify-content: center;
 	align-items: center;
-	width: 35vw;
+	width: 25vw;
 	margin: 0rem 2rem;
 	@media (max-width: 1000px) {
 		width: 80vw;
