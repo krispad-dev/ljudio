ee53df31347bf0b910a0df0171bc1826b6a00703
krispad-dev <kristofer.padoan@icloud.com>
2021-09-28 10:04:35 +0200

    added marqee

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ function Footer() {
 
 	return (
 		<footer>
-{/* 			{auth && auth.loggedIn && <CurrentSongDisplayer />} */}
+			{auth && auth.loggedIn && <CurrentSongDisplayer />}
 			<PlayerController />
 		</footer>
 	);
